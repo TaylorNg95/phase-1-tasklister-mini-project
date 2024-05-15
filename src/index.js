3ef897a1022f540752d96a73ff9ccb0dfb56deee
Taylor Ng <taylormng95@gmail.com>
2024-05-15 17:09:10 -0400

    completes tasklister lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    const newToDo = document.querySelector('#new-task-description').value;
+    const newLi = createToDo(newToDo);
+    document.querySelector('#tasks').appendChild(newLi);
+    form.reset();
+  })
+
+  function createToDo(todo){
+    const li = document.createElement('li');
+    li.textContent = `${todo} `;
+    const btnX = document.createElement('button');
+    btnX.textContent = 'X';
+    btnX.addEventListener('click', removeToDo)
+    li.appendChild(btnX);
+    return li;
+  };
+
+  function removeToDo(todo){
+    todo.target.parentNode.remove();
+  }
 });
